115157e50522b47921a1ae0dc9dc76be62246614
vikram mani <vikram.mani@foodhub.com>
2023-05-24 21:40:51 +0530

    media changes by vikram

---
+++ app/src/main/java/com/baharudin/webviews/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.baharudin.webviews
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.baharudin.webviews.ui.theme.WebviewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebviewsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    WebviewScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebviewScreen(){
+    var backEnable by remember { mutableStateOf(false) }
+    var webView : WebView? = null
+
+    AndroidView(
+        modifier = Modifier,
+        factory = {context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                webViewClient = object : WebViewClient(){
+                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                        backEnable = view!!.canGoBack()
+                    }
+                    /*override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                        if (Uri.parse(url).host == "https://t.me/") {
+                            // Open internal links within the WebView
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                            view.context.startActivity(intent)
+                            return true
+                        }
+                        return true
+                    }*/
+
+                }
+                settings.allowUniversalAccessFromFileURLs = true
+                settings.allowFileAccessFromFileURLs = true
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+                loadUrl("https://logykinfotech.com/login.php")
+                webView = this
+            }
+        }, update = {
+            webView = it
+        })
+
+    BackHandler(enabled = backEnable) {
+        webView?.goBack()
+    }
+}
